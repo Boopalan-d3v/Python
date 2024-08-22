950ba3640b50ac46fb32ffbe32c5532f7a9e4a9d
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-22 10:58:41 +0530

    Add two values without + sign

--- addTwoNum.py
+++ addTwoNum.py
@@ -1,8 +1,8 @@
 x = 6
 y = 8
 
-while y != 0 :
-    temp = x&y
-    x = x^y
+while y != 0 : #checking y not equal to 0 condition: True
+    temp = x&y #concat x & y and store on temp
+    x = x^y 
     y=temp<<1
 print(x)
