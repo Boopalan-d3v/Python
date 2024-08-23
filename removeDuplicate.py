a77ff86d7c27a88b118d24acd3c93581db995541
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-23 17:05:42 +0530

    Remove duplicate element from a list

---
+++ removeDuplicate.py
@@ -0,0 +1,8 @@
+hello = [1, 2, 3, 1, 4, 5, 2]
+print(list(set(hello)))
+
+# Remove the items that are duplicated in two lists 
+
+hell = [3, 4, 7, 2, 2, 1]
+world = [9, 8, 8, 7]
+print(list(set(hell) ^ set(world)))
