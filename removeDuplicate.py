7d257704be46702451341d80b5da1ec553106546
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-23 17:29:29 +0530

    Remove duplicate element from a list

--- removeDuplicate.py
+++ removeDuplicate.py
@@ -6,3 +6,14 @@ print(list(set(hello)))
 hell = [3, 4, 7, 2, 2, 1]
 world = [9, 8, 8, 7]
 print(list(set(hell) ^ set(world)))
+
+# Without arrenging in ascending order 
+
+def Remove(duplicate):
+    final = []
+    for num in duplicate:
+        if  num not in final:
+            final.append(num)
+    return final
+duplicate = [2,3,1,4,1,6,7,4,2,1,9,0]
+print(Remove(duplicate))
