e58d64b0f20a3478784c7370ef293d38a4a6e8ac
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-09-03 09:57:56 +0530

    Fix the error

--- addTwoNum.py
+++ addTwoNum.py
@@ -2,7 +2,7 @@ x = 6
 y = 8
 
 while y != 0 : #checking y not equal to 0 condition: True
-    temp = x&y #concat x & y and store on temp
+    temp = x&y #do bitwise AND of x & y and store on temp
     x = x^y 
     y=temp<<1
 print(x)
